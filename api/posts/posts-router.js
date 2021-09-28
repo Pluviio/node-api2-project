10bf768575c04d303fdd0aeef1f7e629708f2b9d
courtneyharris <harriscourtney98@gmail.com>
2021-09-28 15:57:34 -0400

    Work has begun

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,17 @@
 // implement your posts router here
+const express = require('express')
+const Posts = require('./postss-model')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Posts.find(req.query)
+    .then(posts => {
+        res.status(200).json(posts);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error retrieving posts',
+        })
+    })
+})
--- api/server.js
+++ api/server.js
@@ -1,2 +1,4 @@
 // implement your server here
+const express = require('express');
+const postsRouter = require
 // require your posts router and connect it here
