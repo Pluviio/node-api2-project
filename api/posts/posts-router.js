ec0c2a81cafdb2770ad9dc8da461b2cdcfdc33b9
courtneyharris <harriscourtney98@gmail.com>
2021-09-28 16:13:31 -0400

    Work is done but now to test

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,13 +5,91 @@ const router = express.Router()
 
 router.get('/', (req, res) => {
     Posts.find(req.query)
-    .then(posts => {
-        res.status(200).json(posts);
-    })
-    .catch(error => {
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: 'Error retrieving posts',
+            })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+        .then(posts => {
+            if (posts) {
+                res.status(200).json(posts);
+            } else {
+                res.status(404).json({ message: 'Post not found' });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: 'Error retrieving posts',
+            });
+        });
+});
+
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const { id } = req.params
+        const comments = await Posts.findComments(id)
+        res.status(200).json(comments)
+    } catch (err) {
+        res.status(500).json({
+            message: err.message,
+        })
+    }
+});
+
+router.post('/', (req, res) => {
+    Posts.add(req.body)
+        .then(posts => {
+            res.status(201).json(posts);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: 'Error adding a post',
+            });
+        });
+});
+
+router.delete('/:id', (req, res) => {
+    Posts.remove(req.params.id)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: 'The post has been deleted' });
+            } else {
+                res.status(404).json({ message: 'The post could not be found' });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                message: 'Error removing the post',
+            });
+        });
+});
+
+router.put('/:id', (req, res) => {
+    const changes = req.body;
+    Posts.update(req.params.id, changes)
+      .then(posts => {
+        if (posts) {
+          res.status(200).json(posts);
+        } else {
+          res.status(404).json({ message: 'The post could not be found' });
+        }
+      })
+      .catch(error => {
         console.log(error);
         res.status(500).json({
-            message: 'Error retrieving posts',
-        })
-    })
-})
+          message: 'Error updating the post',
+        });
+      });
+  });
+module.exports = router
